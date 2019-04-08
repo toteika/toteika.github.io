439f89ff5337bbee1727181131abdf4308248af6
toteika <49278686+toteika@users.noreply.github.com>
2019-04-08 22:09:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,12 @@
-<script type="text/javascript">
-            function chg1 (id)
-            {
-                var el = document.getElementById(id);
-                var all = new Array();
-                var num = 0;
-                all[0] = "1.jpg";
-                all[1] = "2.jpg";
-                
-                for (var i=0; i<2; i++)
-                {
-                    if (el.src == all[i])
-                    {
-                        num == i;
-                        break;
-                    }
-                }
-                num++;
-                el.src=all[num];
-            }   
-        </script>
+$(document).ready( function() {
+$('#imgs').hide(1);
+  var path = $('#imgs img').attr('src');
+	$('#back').click(function(){
+	  $('#homeimg').animate({opacity: 1},100,function(){
+	    $(this).html('<img src=' + path + ' />').find('img');
+	  });
+	  return false;
+	});
+
+
+}); // End of ready
