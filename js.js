d87a1307cf96f1c927904b2fa9920f16a36b3fb4
toteika <49278686+toteika@users.noreply.github.com>
2019-04-09 17:46:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.menulink').click(function(){
-    $("#bg").attr('src',"img/1.jpg");
+    $("#img_box").attr('src',"img/1.jpg");
   });
 });
 <a href="" title="Switch" class="menulink">switch me</a>
-    <img src="img/2.jpg" id="imgbox" />
+    <img src="img/2.jpg" id="img_box" />
