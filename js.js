1c8f988e6ee9fac1d8a376130a4dc09c93f1261d
toteika <49278686+toteika@users.noreply.github.com>
2019-04-09 17:42:37 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,8 +2,7 @@
 $(document).ready(function() {
 	// Привязываем событие "click" - нажатие, к кнопке с id="mybutton"
 	$(document).on("click", "#mybutton", function() {
-		// Привязываем функцию "toggle" к элементу с тэгом <img>
-		// содержащимся в элементе с id="img_box"
+		
 		
 	});
 });
