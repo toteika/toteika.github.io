500b1f7a752696b5eeae3b90f4ba345ed57a5a5e
toteika <49278686+toteika@users.noreply.github.com>
2019-04-10 11:19:42 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-// Загрузка скрипта после полной загрузки страницы
+/*// Загрузка скрипта после полной загрузки страницы
 $(document).ready(function() {
 	// Привязываем событие "click" - нажатие, к кнопке с id="mybutton"
 	$(document).on("click", "#mybutton", function() {
@@ -7,3 +7,17 @@ $(document).ready(function() {
 		$("#img_box>img").toggle();
 	});
 });
+/*
+function changepic() {
+  if (document.getElementById('myImg').src == "1.jpg") {
+    var src = "2.jpg";
+  //  $("#text").text("Cобачка2");
+  }
+  else
+  {
+    var src = "1.jpg";
+   // $("#text").text("Cобачка1");
+  }
+  document.getElementById('myImg').src=src
+  // $("#my_image").attr("src", src);
+}
