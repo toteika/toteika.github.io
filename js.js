7d3077cbf24dbb940b9e7d2d1de2958953c2bb0f
toteika <49278686+toteika@users.noreply.github.com>
2019-04-10 11:26:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,3 @@
-/*// Загрузка скрипта после полной загрузки страницы
-$(document).ready(function() {
-	// Привязываем событие "click" - нажатие, к кнопке с id="mybutton"
-	$(document).on("click", "#mybutton", function() {
-		// Привязываем функцию "toggle" к элементу с тэгом <img>
-		// содержащимся в элементе с id="img_box"
-		$("#img_box>img").toggle();
-	});
-});
-/*
 function changepic() {
   if (document.getElementById('myImg').src == "1.jpg") {
     var src = "2.jpg";
